044f40c72b1d4033b688f499bd7dffaad5744276
Vinson Chuong <vinsonchuong@gmail.com>
2016-08-08 07:48:01 -0700

    Add rollup config

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,7 +1,47 @@
-import buildEsnext from 'build-esnext';
+import Directory from 'directory-helpers';
+
+function withProject(test) {
+  return async () => {
+    const project = new Directory('project');
+    try {
+      await test(project);
+    } finally {
+      await project.remove();
+    }
+  };
+}
 
 describe('build-esnext', () => {
-  it('prints Hello World!', () => {
-    expect(buildEsnext()).toBe('Hello World!');
+  it('acts during the compile stage', async () => {
+    const directory = new Directory('.');
+    const {stage} = await directory.read('package.json');
+    expect(stage).toBe('compile');
   });
+
+  xit('compiles ES.next modules', withProject(async (project) => {
+    await project.write({
+      'package.json': {
+        name: 'project'
+      },
+      'src/index.js': `
+        async function sleep(ms) {
+          await new Promise((resolve) => {
+            setTimeout(resolve, ms);
+          });
+        }
+
+        async function run() {
+          await sleep(100);
+          console.log('Hello World!');
+        }
+
+        run();
+      `
+    });
+
+    await project.execJs(`
+      import * as path from 'path';
+      require(path.resolve('../src/index.js')).default();
+    `);
+  }));
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-export default function() {
-  return 'Hello World!';
+import {rollup} from 'rollup';
+
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default async function() {
+  const bundle = await rollup({
+    entry: 'src/index.js',
+    plugins: [
+      nodeResolve({
+        browser: true
+      }),
+      commonjs({
+        sourceMap: false
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+        babelrc: false,
+        presets: ['es2015-rollup', 'stage-0', 'react'],
+        plugins: ['transform-runtime']
+      }),
+
+      uglify()
+    ]
+  });
+
+  await bundle.write({
+    format: 'iife',
+    dest: 'dist/dist.js'
+  });
 }
